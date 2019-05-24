601f767c4b482842eaf0e6125217ca9773a7ef9e
AndroidX Test Team <noreply@google.com>
2019-05-24 12:06:09 -0700

    Migrate Correspondence subclasses to instead call Correspondence.from.
    This makes the code shorter, and the subclassing approach is deprecated.
    
    Open-source note: Correspondence.from was added to Truth 0.43. (So was Correspondence.transforming, which I'm using occasionally instead.) Correspondence.formattingDiffsUsing was added in Truth 0.44.
    
    PiperOrigin-RevId: 249875632

--- ext/truth/java/androidx/test/ext/truth/content/IntentCorrespondences.java
+++ ext/truth/java/androidx/test/ext/truth/content/IntentCorrespondences.java
@@ -16,58 +16,36 @@ public final class IntentCorrespondences {
   private IntentCorrespondences() {}
 
   public static Correspondence<Intent, Intent> action() {
-    return new Correspondence<Intent, Intent>() {
-      @Override
-      public boolean compare(Intent actual, Intent expected) {
-        return Objects.equal(actual.getAction(), expected.getAction());
-      }
-
-      @Override
-      public String toString() {
-        return "has getAction() equal to";
-      }
-    };
+    return Correspondence.from(
+        (actual, expected) -> Objects.equal(actual.getAction(), expected.getAction()),
+        "has getAction() equal to");
   }
 
   public static Correspondence<Intent, Intent> data() {
-    return new Correspondence<Intent, Intent>() {
-      @Override
-      public boolean compare(Intent actual, Intent expected) {
-        return Objects.equal(actual.getData(), expected.getData());
-      }
-
-      @Override
-      public String toString() {
-        return "has getData() equal to";
-      }
-    };
+    return Correspondence.from(
+        (actual, expected) -> Objects.equal(actual.getData(), expected.getData()),
+        "has getData() equal to");
   }
 
   @Beta
   public static Correspondence<Intent, Intent> all(
       final Correspondence<Intent, Intent>... correspondences) {
-    return new Correspondence<Intent, Intent>() {
-      @Override
-      public boolean compare(Intent actual, Intent expected) {
-        for (Correspondence<Intent, Intent> innerCorrespondence : correspondences) {
-          if (!innerCorrespondence.compare(actual, expected)) {
-            return false;
-          }
-        }
-        return true;
+    StringBuilder combinedString = new StringBuilder();
+    for (int i = 0; i < correspondences.length; i++) {
+      combinedString.append(correspondences[i]);
+      if ((i + 1) < correspondences.length) {
+        combinedString.append(" and ");
       }
-
-      @Override
-      public String toString() {
-        StringBuilder combinedString = new StringBuilder();
-        for (int i = 0; i < correspondences.length; i++) {
-          combinedString.append(correspondences[i]);
-          if ((i + 1) < correspondences.length) {
-            combinedString.append(" and ");
+    }
+    return Correspondence.from(
+        (actual, expected) -> {
+          for (Correspondence<Intent, Intent> innerCorrespondence : correspondences) {
+            if (!innerCorrespondence.compare(actual, expected)) {
+              return false;
+            }
           }
-        }
-        return combinedString.toString();
-      }
-    };
+          return true;
+        },
+        combinedString.toString());
   }
 }
