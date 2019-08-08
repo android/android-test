0e27bf1dba01e232368dfb3f603504fabe0aee80
AndroidX Test Team <noreply@google.com>
2019-08-08 16:51:50 -0700

    Test Discovery service implementation
    PiperOrigin-RevId: 262461079

--- services/events/java/androidx/test/services/events/AnnotationValue.java
+++ services/events/java/androidx/test/services/events/AnnotationValue.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package androidx.test.services.events.discovery;
+package androidx.test.services.events;
 
 import android.os.Parcel;
 import android.os.Parcelable;
