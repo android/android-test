d107c5f217b9d8763f8d761a264c590a55804a1d
AndroidX Test Team <noreply@google.com>
2019-08-22 15:25:35 -0700

    Test event client for new orchestrator
    PiperOrigin-RevId: 264932425

---
+++ services/events/java/androidx/test/services/events/client/OrchestratorConnection.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.test.services.events.client;
+
+import android.content.Context;
+
+/**
+ * Connects to the remote service E.g. {@code androidx.test.orchestrator.OrchestratorService} and
+ * notifies {@link androidx.test.runner.AndroidJUnitRunner} (the caller) when the connection is
+ * established via the {@link TestEventClientConnectListener} interface.
+ */
+public interface OrchestratorConnection {
+
+  /**
+   * Connects/binds to the service. The connect operation is asynchronous, so the caller needs to
+   * wait for {@link TestEventClientConnectListener#onTestEventClientConnect()} before attempting to
+   * use the service.
+   *
+   * @param context the instrumentation {@link Context} to use for binding to the service
+   */
+  void connect(Context context);
+}
