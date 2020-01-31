d8f65b561d04f7d0d63fbab84aa257c9bd7c6d89
AndroidX Test Team <noreply@google.com>
2020-01-31 08:35:35 -0800

    Migrate org.mockito.Matchers#any to org.mockito.ArgumentMatchers
    The former are deprecated and replaced by the latter in Mockito 2.
    
    PiperOrigin-RevId: 292550778

--- runner/monitor/javatests/androidx/test/internal/runner/InstrumentationConnectionTest.java
+++ runner/monitor/javatests/androidx/test/internal/runner/InstrumentationConnectionTest.java
@@ -24,7 +24,7 @@ import static junit.framework.Assert.assertNull;
 import static junit.framework.Assert.assertTrue;
 import static junit.framework.Assert.fail;
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Matchers.any;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 
