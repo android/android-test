acad5c0ff25c4434fdf1de4fb4ba525869f3a394
AndroidX Test Team <copybara-androidxtest@google.com>
2021-01-06 14:24:26 -0800

    InstrumentationActivityInvoker: Add FLAG_IMMUTABLE to PIs
    PiperOrigin-RevId: 350427635

--- core/java/androidx/test/core/app/InstrumentationActivityInvoker.java
+++ core/java/androidx/test/core/app/InstrumentationActivityInvoker.java
@@ -418,7 +418,7 @@ class InstrumentationActivityInvoker implements ActivityInvoker {
                     getApplicationContext(),
                     /*requestCode=*/ 0,
                     intent,
-                    /*flags=*/ PendingIntent.FLAG_UPDATE_CURRENT))
+                    /*flags=*/ PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE))
             .putExtra(TARGET_ACTIVITY_OPTIONS_BUNDLE_KEY, activityOptions);
 
     if (Build.VERSION.SDK_INT < 16) {
