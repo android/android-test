2efe511d16704f3ff4d53d1af90bdcdfda01bf8e
AndroidX Test Team <copybara-androidxtest@google.com>
2021-09-09 13:36:26 -0700

    Convert EspressoDevice and DeviceInteraction to Kotlin
    PiperOrigin-RevId: 395776122

--- espresso/core/java/androidx/test/espresso/device/DeviceInteraction.kt
+++ espresso/core/java/androidx/test/espresso/device/DeviceInteraction.kt
@@ -13,14 +13,12 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package androidx.test.espresso.device;
+package androidx.test.espresso.device
 
-import static com.google.common.base.Preconditions.checkNotNull;
-
-import androidx.test.annotation.ExperimentalTestApi;
-import androidx.test.espresso.device.action.DeviceAction;
-import androidx.test.espresso.device.action.DeviceController;
-import javax.inject.Inject;
+import androidx.test.annotation.ExperimentalTestApi
+import androidx.test.espresso.device.action.DeviceAction
+import androidx.test.espresso.device.action.DeviceController
+import javax.inject.Inject
 
 /**
  * API surface for performing device-centric operations.
@@ -28,13 +26,7 @@ import javax.inject.Inject;
  * <p>This API is experimental and subject to change.
  */
 @ExperimentalTestApi
-public class DeviceInteraction {
-  private final DeviceController deviceController;
-
-  @Inject
-  DeviceInteraction(DeviceController deviceController) {
-    this.deviceController = deviceController;
-  }
+class DeviceInteraction @Inject constructor(private val deviceController: DeviceController) {
 
   /**
    * Performs the given action on the test device.
@@ -42,9 +34,9 @@ public class DeviceInteraction {
    * @param action the DeviceAction to execute.
    * @return this interaction for further perform/verification calls.
    */
-  public DeviceInteraction perform(DeviceAction action) {
-    checkNotNull(action);
-    action.perform(deviceController);
-    return this;
+  fun perform(action: DeviceAction): DeviceInteraction {
+    requireNotNull(action)
+    action.perform(deviceController)
+    return this
   }
 }
