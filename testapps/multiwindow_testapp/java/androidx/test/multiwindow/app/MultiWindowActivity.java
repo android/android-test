be2fde680c415bae9e852697598266f1385ba72d
AndroidX Test Team <copybara-androidxtest@google.com>
2022-04-27 11:10:41 -0700

    Create test app for multi-window mode integration tests
    PiperOrigin-RevId: 444914775

---
+++ testapps/multiwindow_testapp/java/androidx/test/multiwindow/app/MultiWindowActivity.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.test.multiwindow.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+/** Activity that updates a TextView when entering or exiting multi-window mode. */
+public class MultiWindowActivity extends Activity {
+  private static final String TAG = MultiWindowActivity.class.getSimpleName();
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.multiwindow_activity);
+
+    String multiWindowModeText;
+    if (this.isInMultiWindowMode()) {
+      multiWindowModeText = "Device is in multi-window mode.";
+    } else {
+      multiWindowModeText = "Device is not in multi-window mode.";
+    }
+    Log.d(TAG, "onCreate. " + multiWindowModeText);
+    TextView textView = (TextView) findViewById(R.id.multi_window_mode);
+    textView.setText(multiWindowModeText);
+  }
+}
