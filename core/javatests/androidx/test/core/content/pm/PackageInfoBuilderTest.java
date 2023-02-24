fee90aa8618991b590daf88aa950b8a8b1d4fd35
AndroidX Test Team <copybara-androidxtest@google.com>
2023-02-24 14:10:02 -0800

    // DUE TO DIFFBASE
    PiperOrigin-RevId: 512159665

--- core/javatests/androidx/test/core/content/pm/PackageInfoBuilderTest.java
+++ core/javatests/androidx/test/core/content/pm/PackageInfoBuilderTest.java
@@ -17,8 +17,10 @@ package androidx.test.core.content.pm;
 
 import static com.google.common.truth.Truth.assertThat;
 import static junit.framework.Assert.fail;
+import static org.junit.Assume.assumeTrue;
 
 import android.content.pm.PackageInfo;
+import android.os.Build;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -29,6 +31,11 @@ public final class PackageInfoBuilderTest {
 
   private static final String TEST_PACKAGE_NAME = "test.package.name";
   private static final String SECOND_TEST_PACKAGE_NAME = "second.test.package.name";
+  private static final String[] TEST_REQUESTED_PERMISSIONS = {
+    "android.permission.test.one", "android.permission.test.two"
+  };
+  private static final int TEST_VERSION_CODE = 123;
+  private static final String TEST_VERSION_NAME = "v1";
 
   @Test
   public void buildAllFields() {
@@ -37,10 +44,16 @@ public final class PackageInfoBuilderTest {
             .setPackageName(TEST_PACKAGE_NAME)
             .setApplicationInfo(
                 ApplicationInfoBuilder.newBuilder().setPackageName(TEST_PACKAGE_NAME).build())
+            .setRequestedPermissions(TEST_REQUESTED_PERMISSIONS)
+            .setVersionCode(TEST_VERSION_CODE)
+            .setVersionName(TEST_VERSION_NAME)
             .build();
 
     assertThat(packageInfo.packageName).isEqualTo(TEST_PACKAGE_NAME);
     assertThat(packageInfo.applicationInfo).isNotNull();
+    assertThat(packageInfo.requestedPermissions).isEqualTo(TEST_REQUESTED_PERMISSIONS);
+    assertThat(packageInfo.versionCode).isEqualTo(TEST_VERSION_CODE);
+    assertThat(packageInfo.versionName).isEqualTo(TEST_VERSION_NAME);
   }
 
   @Test
@@ -80,6 +93,28 @@ public final class PackageInfoBuilderTest {
       assertThat(e)
           .hasMessageThat()
           .isEqualTo("Field 'packageName' must match field 'applicationInfo.packageName'");
+    }
+  }
+
+  @Test
+  public void build_throwsException_whenPermissionsFlagsArraySizeMismatched() {
+    // API added in API 16 or newer.
+    assumeTrue(Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN);
+    PackageInfoBuilder builder =
+        PackageInfoBuilder.newBuilder()
+            .setPackageName(TEST_PACKAGE_NAME)
+            .setApplicationInfo(
+                ApplicationInfoBuilder.newBuilder().setPackageName(TEST_PACKAGE_NAME).build())
+            .setRequestedPermissions(TEST_REQUESTED_PERMISSIONS)
+            .setRequestedPermissionsFlags(new int[] {PackageInfo.REQUESTED_PERMISSION_GRANTED});
+
+    try {
+      builder.build();
+      fail();
+    } catch (IllegalStateException e) {
+      assertThat(e)
+          .hasMessageThat()
+          .isEqualTo("Field 'requestedPermissions' must match size of 'requestedPermissionsFlags'");
     }
   }
 }
