cb2bea322e8bd55bb685c4efd6285a46556437ad
Paige McAuliffe <paigemca@google.com>
2023-07-17 12:14:51 -0700

    Use MainScope for tracking window layout info in BaseSingleFoldDeviceAction
    PiperOrigin-RevId: 548763780

--- espresso/device/java/androidx/test/espresso/device/action/BaseSingleFoldDeviceAction.kt
+++ espresso/device/java/androidx/test/espresso/device/action/BaseSingleFoldDeviceAction.kt
@@ -30,7 +30,7 @@ import androidx.window.layout.WindowLayoutInfo
 import java.util.concurrent.CountDownLatch
 import java.util.concurrent.Executor
 import java.util.concurrent.TimeUnit
-import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.asCoroutineDispatcher
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.distinctUntilChanged
@@ -62,7 +62,7 @@ internal open class BaseSingleFoldDeviceAction(
     val latch: CountDownLatch = CountDownLatch(1)
     val windowInfoTracker = WindowInfoTracker.getOrCreate(activity)
 
-    CoroutineScope(mainExecutor.asCoroutineDispatcher()).launch {
+    MainScope().launch {
       windowInfoTracker.windowLayoutInfo(activity).distinctUntilChanged().collect {
         windowLayoutInfo: WindowLayoutInfo ->
         val foldingFeatures = windowLayoutInfo.displayFeatures.filterIsInstance<FoldingFeature>()
