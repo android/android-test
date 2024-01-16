1ae53b93e02cc363311f6564a35edeea1b075103
Brett Chabot <brettchabot@google.com>
2024-01-16 15:52:21 -0800

    Use bazel legacy-test-34 instead of 30.
    The github VMs apparently no longer have the 30 sdk installed by default.
    
    PiperOrigin-RevId: 598983639

--- kokoro/build_artifacts.sh
+++ kokoro/build_artifacts.sh
@@ -9,8 +9,6 @@ cd "${KOKORO_ARTIFACTS_DIR}/github/android-test-releases"
 
 source kokoro/common.sh
 install_bazelisk
-# runner has @androidsdk//:legacy_test-30 as a dependency
-/opt/android-sdk/cmdline-tools/latest/bin/sdkmanager --sdk_root=/opt/android-sdk "platforms;android-30" "build-tools;30.0.0"
 # building :axt_m2_repository uses @androidsdk//:build-tools/33.0.2/aapt2
 /opt/android-sdk/cmdline-tools/latest/bin/sdkmanager --sdk_root=/opt/android-sdk "build-tools;33.0.2"
 /opt/android-sdk/cmdline-tools/latest/bin/sdkmanager --sdk_root=/opt/android-sdk "platforms;android-34" "build-tools;34.0.0"
