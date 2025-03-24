59f7645d2e9a1c73da09c7c2dd429127c30b7328
Brett Chabot <brettchabot@google.com>
2025-03-24 13:27:04 -0700

    Use JDK 21 for test actions too.
    PiperOrigin-RevId: 740050574

--- kokoro/test_artifacts.sh
+++ kokoro/test_artifacts.sh
@@ -11,8 +11,8 @@ unzip "${KOKORO_GFILE_DIR}/axt_m2repository.zip" -d "/root/.m2"
 curl -s https://repos.azul.com/azul-repo.key | sudo gpg --dearmor -o /usr/share/keyrings/azul.gpg
 echo "deb [signed-by=/usr/share/keyrings/azul.gpg] https://repos.azul.com/zulu/deb stable main" | sudo tee /etc/apt/sources.list.d/zulu.list
 apt-get update
-apt-get install -y zulu17-jdk
-export JAVA_HOME="$(update-java-alternatives -l | grep "1.17" | head -n 1 | tr -s " " | cut -d " " -f 3)"
+apt-get install -y zulu21-jdk
+export JAVA_HOME="$(update-java-alternatives -l | grep "1.21" | head -n 1 | tr -s " " | cut -d " " -f 3)"
 
 cd gradle-tests
 ./gradlew nexusOneDebugAndroidTest --stacktrace -Pandroid.testoptions.manageddevices.emulator.gpu=swiftshader_indirect -Dandroid.experimental.androidTest.numManagedDeviceShards=1
